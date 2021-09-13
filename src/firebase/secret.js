fa64192de390d69d71efbeaad824fb995ac5f971
Abhay Chauhan <chauhan27.abhay@gmail.com>
2021-09-13 12:00:14 +0530

    firebase deploy

--- src/firebase/secret.js
+++ src/firebase/secret.js
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyB7CqlfS-lxKxmJkLujlHQM0PelJFSmKiE",
-    authDomain: "resume-builder-f0dc3.firebaseapp.com",
-    projectId: "resume-builder-f0dc3",
-    storageBucket: "resume-builder-f0dc3.appspot.com",
-    messagingSenderId: "645688620967",
-    appId: "1:645688620967:web:3c19c19155a0736fcfe882"
-  };
+  apiKey: "AIzaSyB7CqlfS-lxKxmJkLujlHQM0PelJFSmKiE",
+  authDomain: "resume-builder-f0dc3.firebaseapp.com",
+  projectId: "resume-builder-f0dc3",
+  storageBucket: "resume-builder-f0dc3.appspot.com",
+  messagingSenderId: "645688620967",
+  appId: "1:645688620967:web:3c19c19155a0736fcfe882"
+};
